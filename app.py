113a8ec8ec0ba9adf1971dd3ddf1ef99735f22f7
naveen3830 <naveenr3830@gmail.com>
2024-06-10 19:29:53 +0530

    updated requirements file

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Load the GROQ and Google API keys
 groq_api_key = os.getenv('GROQ_API_KEY')
 # os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
-
+groq_api_key="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"
 # Initialize the language model
 llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192", temperature=1)
 

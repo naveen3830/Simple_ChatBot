37fc4ce35d277e7035464570c8cbed530043bbcb
naveen3830 <naveenr3830@gmail.com>
2024-06-10 13:24:49 +0530

    Added requirements file

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 import os
+
+# Load environment variables
 load_dotenv()
 
-## load the GROQ And OpenAI API KEY 
-groq_api_key=os.getenv('GROQ_API_KEY')
-os.environ["GOOGLE_API_KEY"]=os.getenv("GOOGLE_API_KEY")
+# Load the GROQ and Google API keys
+groq_api_key = os.getenv('GROQ_API_KEY')
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
-llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192",temperature=1)
+# Initialize the language model
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192", temperature=1)
 
+# Set the header of the Streamlit app
+st.header('Simple :blue[_Chat-Bot_] :sunglasses:', divider='rainbow')
 
-
-
-if selected=="Simple Chat-Bot":
-    st.header('Simple :blue[_Chat-Bot_] :sunglasses:',divider='rainbow')
-    def generate_result():
-        prompt=st.chat_input("Type something here")
+def generate_result():
+    prompt = st.chat_input("Type something here")
+    
+    if prompt:
+        response = llm.invoke(prompt)
+        st.markdown(response.content)
         
-        if prompt:
-            response=llm.invoke(prompt)
-            st.markdown(response.content)
+        
+        # num_lines = generated_text.count('\n') + 1
+        # st.text_area("Generated Response", generated_text, height=num_lines * 20)
 
-    if __name__ == "__main__":
-        generate_result()
+if __name__ == "__main__":
+    generate_result()

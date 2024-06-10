7ec54b1b90d8c3ebd48d6bf721d638a3c974cf4b
naveen3830 <naveenr3830@gmail.com>
2024-06-10 19:44:18 +0530

    updated  file

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ import streamlit as st
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 import os
-
 # Load environment variables
 load_dotenv()
+
+headers={
+    "authorization":st.secrets["groq_api_key"],
+    "content-type":"application/json"
+}
 
 # Load the GROQ and Google API keys
 groq_api_key = os.getenv('GROQ_API_KEY')
 # os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
-groq_api_key="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"
+
 # Initialize the language model
 llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192", temperature=1)
 
